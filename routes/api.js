8c1bfc4908e7aec41d8ce3f1aa0feb5bde492348
Fiorela Romero F <fiorelaromerof@gmail.com>
2023-01-16 18:00:52 -0500

    add validations in routes

--- routes/api.js
+++ routes/api.js
@@ -2,16 +2,28 @@ const express = require("express");
 const router = express.Router();
 const api = require("../services/api");
 const { sign } = require("../utils/sign");
+const {
+  validateQueryParam,
+  validateParamId,
+} = require("../middlewares/validations");
 
-router.get("/items", async function (req, res, next) {
-  const result = await api.search({ query: req.query.q });
-  result.author = sign();
-  return res.send(result);
+router.get("/items", validateQueryParam, async function (req, res, next) {
+  try {
+    const result = await api.search({ query: req.query.search });
+    result.author = sign();
+    return res.send(result);
+  } catch (error) {
+    return res.status(500).send({ error: "Internal Server Error" });
+  }
 });
 
-router.get("/items/:id", async function (req, res, next) {
-  const result = await api.productDetail({ id: req.params.id });
-  result.author = sign();
-  return res.send(result);
+router.get("/items/:id", validateParamId, async function (req, res, next) {
+  try {
+    const result = await api.productDetail({ id: req.params.id });
+    result.author = sign();
+    return res.send(result);
+  } catch (error) {
+    return res.status(500).send({ error: "Internal Server Error" });
+  }
 });
 module.exports = router;
